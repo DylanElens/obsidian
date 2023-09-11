99e8fa760741e1ba239eb99c05f9a02d51e5ad44
dylan <dylanelens@hotmail.com>
2023-09-11 11:16:13 +0200

    finish homework week2

--- Functional Programming/week 2/Char.hs
+++ Functional Programming/week 2/Char.hs
@@ -2,13 +2,26 @@ module Char where
 
 import Data.Char
 
---(~~) :: String -> String -> Bool
+(~~) :: String -> String -> Bool
+(~~) x y = map toLower x == map toLower y
 
---reverseCase :: String -> String
+reverseCase :: String -> String
+reverseCase = map reverseCaseWord
 
---shift :: Int -> Char -> Char
+reverseCaseWord :: Char -> Char
+reverseCaseWord c
+  | isUpper c = toLower c
+  | otherwise = toUpper c
 
---caesar :: Int -> String -> String
+shift :: Int -> Char -> Char
+shift n x
+  | x == ' ' = x
+  | ord x < 65 || ord x > 90 = x
+  | (ord x + n) > 90 = chr (((ord x - ord 'A' + n) `mod` 26) + ord 'A')
+  | otherwise = chr (ord x + n)
+
+caesar :: Int -> String -> String
+caesar x = map (shift x . toUpper)
 
 msg :: String
 msg = "ADMNO D HPNO NKMDIFGZ TJP RDOC AVDMT YPNO"
