36e2a49475d3d076c44fac2d144d6ed6090956db
dylan <dylanelens@hotmail.com>
2023-09-27 17:46:54 +0200

    vault backup: 2023-09-27 17:46:54

--- Functional Programming/week 4/Tree.hs
+++ Functional Programming/week 4/Tree.hs
@@ -5,24 +5,89 @@ data Tree a = Leaf | Node a (Tree a) (Tree a)
 
 {----------- exercise 4.3 -------------}
 
---leaves :: Tree a -> Int
---nodes  :: Tree a -> Int
---height :: Tree a -> Int
---elems  :: Tree a -> [a]
---isSearchTree :: (Ord a) => Tree a -> Bool
+leaves :: Tree a -> Int
+leaves Leaf = 1
+leaves (Node _ lt rt) = leaves lt + leaves rt
+
+nodes  :: Tree a -> Int
+nodes Leaf = 0
+nodes (Node _ lt rt) = 1 + nodes lt + nodes rt
+
+height :: Tree a -> Int
+height Leaf = 0
+height (Node _ lt rt) = 1 + max (height lt) (height rt)
+
+elems  :: Tree a -> [a]
+elems Leaf = []
+elems (Node k lt rt) = k : elems lt ++ elems rt
+
+isSearchTree :: (Ord a) => Tree a -> Bool
+isSearchTree Leaf = True
+isSearchTree (Node k lt rt) = isSearchTree lt && isSearchTree rt && go k lt rt
+  where
+    go :: (Ord a) => a -> Tree a -> Tree a -> Bool
+    go _ Leaf Leaf = True
+    go k' Leaf (Node k'' _ _) = k' < k''
+    go k' (Node k'' _ _) Leaf = k' > k''
+    go k' (Node k'' _ _) (Node k''' _ _) = k' > k'' && k' < k'''
 
 {----------- exercise 4.4 -------------}
 
---member :: (Ord a) => a -> Tree a -> Bool
---insert :: (Ord a) => a -> Tree a -> Tree a
---delete :: (Ord a) => a -> Tree a -> Tree a
---fromList :: (Ord a) => [a] -> Tree a
+member :: (Ord a) => a -> Tree a -> Bool
+member _ Leaf = False
+member x (Node k lt rt)
+  | x == k = True
+  | x < k = member x lt
+  | otherwise = member x rt
+
+insert :: (Ord a) => a -> Tree a -> Tree a
+insert x Leaf = Node x Leaf Leaf
+insert x (Node k lt rt)
+  | x == k = Node k lt rt
+  | x < k = Node k (insert x lt) rt
+  | otherwise = Node k lt (insert x rt)
+
+delete :: (Ord a) => a -> Tree a -> Tree a
+delete _ Leaf = Leaf
+delete x (Node k lt rt)
+  | x == k = deleteRoot (Node k lt rt)
+  | x < k = Node k (delete x lt) rt
+  | otherwise = Node k lt (delete x rt)
+  where
+    deleteRoot :: (Ord a) => Tree a -> Tree a
+    deleteRoot (Node _ Leaf rt) = rt
+    deleteRoot (Node _ lt Leaf) = lt
+    deleteRoot (Node _ lt rt) = Node k' lt (delete k' rt)
+      where
+        k' = leftMost rt
+        leftMost :: (Ord a) => Tree a -> a
+        leftMost (Node k Leaf _) = k
+        leftMost (Node _ lt _) = leftMost lt
+
+fromList :: (Ord a) => [a] -> Tree a
+fromList = foldr insert Leaf
 
 {----------- exercise 4.5 -------------}
 
---inOrder :: Tree a -> [a]
---fromAscList :: [a] -> Tree a
---breadthFirst :: Tree a -> [a]
+inOrder :: Tree a -> [a]
+inOrder Leaf = []
+inOrder (Node k lt rt) = inOrder lt ++ [k] ++ inOrder rt
+
+fromAscList :: [a] -> Tree a
+fromAscList [] = Leaf
+fromAscList xs = Node k (fromAscList lt) (fromAscList rt)
+  where
+    (lt, k:rt) = splitAt (length xs `div` 2) xs
+
+breadthFirst :: Tree a -> [a]
+breadthFirst tree = go [tree]
+  where
+    go [] = []
+    go (Leaf:xs) = go xs
+    go (Node k lt rt:xs) = k : go (xs ++ [lt, rt])
+
+
+{----------- exercise 4.6 -------------}
 
 {- BONUS: a tree pretty printer; the recursive structure of this function
  - is prety simple, but it is a fiddly function to write if you want it to
